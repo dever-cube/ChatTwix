6c7f8a5c75509d5e55f6f987bdc9420973670f1c
dever-cube <developed.by.fox@gmail.com>
2023-07-13 10:50:34 +0200

    Add mod configs

--- src/main/java/net/devcube/chattwix/ModInfo.java
+++ src/main/java/net/devcube/chattwix/ModInfo.java
@@ -3,4 +3,5 @@ package net.devcube.chattwix;
 public class ModInfo {
     public static final String MOD_ID = "chattwix";
     public static final String MOD_NAME = "Chat Twix";
+    public static final String MOD_VERSION = "0.0.1-1.20.1";
 }
---
+++ src/main/java/net/devcube/chattwix/config/Configs.java
@@ -0,0 +1,78 @@
+package net.devcube.chattwix.config;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import fi.dy.masa.malilib.config.ConfigUtils;
+import fi.dy.masa.malilib.config.IConfigBase;
+import fi.dy.masa.malilib.config.IConfigHandler;
+import fi.dy.masa.malilib.config.options.ConfigBoolean;
+import fi.dy.masa.malilib.config.options.ConfigColor;
+import fi.dy.masa.malilib.config.options.ConfigDouble;
+import fi.dy.masa.malilib.util.FileUtils;
+import fi.dy.masa.malilib.util.JsonUtils;
+import net.devcube.chattwix.ModInfo;
+
+import java.io.File;
+
+public class Configs implements IConfigHandler {
+
+    private static final String CONFIG_FILE_NAME = ModInfo.MOD_ID + ".json";
+
+    public static class Generic{
+        public static final ConfigBoolean CLOSE_AFTER_SEND = new ConfigBoolean("chattwix.option.close_after_send", true, "chattwix.option.close_after_send.desc");
+        public static final ConfigBoolean PLAY_MSG_SOUND = new ConfigBoolean("chattwix.option.play_msg_sound", false, "chattwix.option.play_msg_sound.desc");
+        public static final ConfigDouble MSG_SOUND_VOLUME = new ConfigDouble("chattwix.option.msg_sound_volume", 0.5, 0.0, 1.0, "chattwix.option.msg_sound_volume.desc");
+
+        public static final ImmutableList<IConfigBase> OPTIONS = ImmutableList.of(
+                CLOSE_AFTER_SEND,
+                PLAY_MSG_SOUND,
+                MSG_SOUND_VOLUME);
+    }
+
+    public static class Style{
+        public static final ConfigBoolean BOLD = new ConfigBoolean("chattwix.option.msg_bold", false, "chattwix.option.msg_bold.desc");
+        public static final ConfigBoolean ITALIC = new ConfigBoolean("chattwix.option.msg_italic", true, "chattwix.option.msg_italic.desc");
+        public static final ConfigColor MSG_COLOR = new ConfigColor("chattwix.option.msg_color", "#AAAAAA", "chattwix.option.msg_color.desc");
+
+        public static final ImmutableList<IConfigBase> OPTIONS = ImmutableList.of(
+                BOLD,
+                ITALIC,
+                MSG_COLOR);
+    }
+
+
+
+    @Override
+    public void load() {
+        File configFile = new File(FileUtils.getConfigDirectory(), CONFIG_FILE_NAME);
+
+        if (configFile.exists() && configFile.isFile() && configFile.canRead())
+        {
+            JsonElement element = JsonUtils.parseJsonFile(configFile);
+
+            if (element != null && element.isJsonObject())
+            {
+                JsonObject root = element.getAsJsonObject();
+                ConfigUtils.readConfigBase(root, "Generic", Configs.Generic.OPTIONS);
+                ConfigUtils.readConfigBase(root, "Style", Configs.Style.OPTIONS);
+
+            }
+        }
+    }
+
+    @Override
+    public void save() {
+        File dir = FileUtils.getConfigDirectory();
+
+        if ((dir.exists() && dir.isDirectory()) || dir.mkdirs())
+        {
+            JsonObject root = new JsonObject();
+            ConfigUtils.writeConfigBase(root, "Generic", Configs.Generic.OPTIONS);
+            ConfigUtils.writeConfigBase(root, "Style", Configs.Style.OPTIONS);
+            root.add("config_version", new JsonPrimitive(ModInfo.MOD_VERSION));
+            JsonUtils.writeJsonToFile(root, new File(dir, CONFIG_FILE_NAME));
+        }
+    }
+}
