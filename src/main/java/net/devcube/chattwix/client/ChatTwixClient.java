fc21c762ff34098ba92974967f2d3507b8a19e7f
dever-cube <developed.by.fox@gmail.com>
2023-07-13 10:24:55 +0200

    Initial commit

---
+++ src/main/java/net/devcube/chattwix/client/ChatTwixClient.java
@@ -0,0 +1,11 @@
+package net.devcube.chattwix.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class ChatTwixClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
